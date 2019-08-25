8176309bb31095c0aaf64ee961526ce80936d071
galhardo <aleexgvieira@gmail.com>
2019-08-24 22:57:46 -0300

    up

--- config.php
+++ config.php
@@ -4,21 +4,20 @@ require 'environment.php';
 // define("BASE", "http://localhost/facebook/");
 define("BASE", "http://social-alexgalhardo-com.umbler.net/")
 
-
 global $config;
 
 $config = array();
 
 if(ENVIRONMENT == 'production') {
-	$config['dbname'] = 'heroku_33ab81938761b04';
-	$config['host'] = 'us-cdbr-iron-east-02.cleardb.net';
-	$config['dbuser'] = 'b305111783c034';
-	$config['dbpass'] = '6e03016d';
+	$config['dbname'] = 'galhardosocial';
+	$config['host'] = 'mysql873.umbler.com';
+	$config['dbuser'] = 'alexgalhardo';
+	$config['dbpass'] = 'galhardo159';
 } else {
 	$config['dbname'] = 'facebook';
 	$config['host'] = 'localhost';
 	$config['dbuser'] = 'root';
-	$config['dbpass'] = 'root';
+	$config['dbpass'] = '';
 }
 ?>
 
