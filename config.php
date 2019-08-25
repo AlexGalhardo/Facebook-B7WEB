c705ebe82fe8d864b25e3e02f509a3d46d067d7f
galhardo <aleexgvieira@gmail.com>
2019-08-24 22:28:06 -0300

    up

--- config.php
+++ config.php
@@ -1,24 +1,25 @@
 <?php
-// require 'environment.php';
+require 'environment.php';
 
 // define("BASE", "http://localhost/facebook/");
-define("BASE", "https://galhardo-social.herokuapp.com/")
-$config = array();
-$config['dbname'] = 'heroku_33ab81938761b04';
-$config['host'] = 'us-cdbr-iron-east-02.cleardb.net';
-$config['dbuser'] = 'b305111783c034';
-$config['dbpass'] = '6e03016d';
+define("BASE", "http://social-alexgalhardo-com.umbler.net/")
 
-/*
+
+global $config;
+
+$config = array();
+
 if(ENVIRONMENT == 'production') {
-	
+	$config['dbname'] = 'heroku_33ab81938761b04';
+	$config['host'] = 'us-cdbr-iron-east-02.cleardb.net';
+	$config['dbuser'] = 'b305111783c034';
+	$config['dbpass'] = '6e03016d';
 } else {
 	$config['dbname'] = 'facebook';
 	$config['host'] = 'localhost';
 	$config['dbuser'] = 'root';
 	$config['dbpass'] = 'root';
 }
-*/
 ?>
 
 
